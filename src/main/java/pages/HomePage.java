161bf7ccebf3e61e7641404dc60f4759c5db7312
AchiniUresha <wpachiniuresha@gmail.com>
2025-06-29 22:02:56 +0530

    Merge remote-tracking branch 'githuB/master'

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,17 +1,16 @@
 package pages;//package pages;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class HomePage {
     WebDriver driver;
 
+    // Locators
     private By createBoardTitle = By.xpath("//div[@data-cy='create-board']");
     private By boardTitleInput = By.xpath("//input[@class='new-board-input']");
     private By boardHeader = By.xpath("//input[@name='board-title']");
@@ -22,17 +21,24 @@ public class HomePage {
         this.driver = driver;
    }
 
+    // Click on the 'Create New Board' section
    public void clickCreateNewBoard(){
         driver.findElement(createBoardTitle).click();
-       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-       wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='new-board-input']")));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='new-board-input']")));
    }
 
+    /**
+     * Enters a board name and hits Enter to create it.
+     *
+     * @param boardName the name of the board to be created
+     */
    public void enterBoardNameAndSubmit(String boardName){
         driver.findElement(boardTitleInput).sendKeys(boardName+ Keys.ENTER);
 
    }
 
+    // Get the board title
     public String getBoardTitleOnTopCorner() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(boardHeader));
